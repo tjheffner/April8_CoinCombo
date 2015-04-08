1fc7295217b2af3a29119d77512e7f0ab0dfe9ae
Bryan Borgeson & Tanner Heffner <student@epicodus.com>
2015-04-08 10:05:12 -0700

    write 4 tests and make pass for dimes, nickels, and pennies

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var coinCombo = function(change) {
+
+    var coins = [0, 0, 0, 0];
+
+    if (change >= 0.10) {
+        var dimes = (change / 0.10);
+        coins[1] = dimes;
+        change -= (dimes * 0.10);
+    } else if (change >= 0.05) {
+        var nickels = (change / 0.05);
+        coins[2] = nickels;
+        change -= (nickels * 0.05);
+    } else {
+//pennies
+    coins[3] = (change / 0.01);
+    }
+//    var dimes = 0;
+//    var quarters = 0;
+
+    // nickels = (change / .05);
+    // pennies = (change / .01);
+    //
+    //
+    // if ((change % .05) > 0) {
+    //     pennies = ((change % .05) / .01);
+    // }
+
+console.log(coins);
+    return coins;
+
+};
---
+++ specs/specs.js
@@ -0,0 +1,17 @@
+describe("coinCombo", function() {
+    it("will return [0, 0, 0, 1] for an input of .01", function() {
+        expect(coinCombo(.01)).to.eql([0, 0, 0, 1]);
+    });
+
+    it("will return [0, 0, 0, 2] for an input of .02", function() {
+        expect(coinCombo(.02)).to.eql([0, 0, 0, 2]);
+    });
+
+    it("will return [0, 0, 1, 0] for an input of .05", function() {
+        expect(coinCombo(.05)).to.eql([0, 0, 1, 0]);
+    });
+
+    it("will return [0, 1, 0, 0] for an input of .1", function() {
+        expect(coinCombo(.1)).to.eql([0, 1, 0, 0]);
+    });
+});
