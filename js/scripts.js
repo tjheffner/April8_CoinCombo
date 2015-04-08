cf839a1c876ba8265f59742e7b0bc124334d82b8
Bryan Borgeson & Tanner Heffner <student@epicodus.com>
2015-04-08 11:24:25 -0700

    add css and jquery logic to control for what is shown to the user after submission

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,73 @@ var coinCombo = function(change) {
 
     var cents = change * 100;
 
-    do {
-        switch (cents > 0) {
-            //quarter
-             case (cents >= 25):
-                var quarters = Math.floor(cents / 25);
-                coins[0] = quarters;
-                cents -= (quarters * 25);
-            //dimes
-            case (cents >= 10):
-                var dimes = Math.floor(cents / 10);
-                coins[1] = dimes;
-                cents -= (dimes * 10);
-            //nickels
-            case (cents >= 05):
-                var nickels = Math.floor(cents / 05);
-                coins[2] = nickels;
-                cents -= (nickels * 05);
-            //pennies
-            case (cents >= 01):
-                var pennies = Math.floor(cents / 01);
-                coins[3] = pennies;
-                cents = 0;
-                // cents -= (pennies * 01);
-                break;
-        }
+    switch (cents > 0) {
+        //quarter
+         case (cents >= 25):
+            var quarters = Math.floor(cents / 25);
+            coins[0] = quarters;
+            cents -= (quarters * 25);
+        //dimes
+        case (cents >= 10):
+            var dimes = Math.floor(cents / 10);
+            coins[1] = dimes;
+            cents -= (dimes * 10);
+        //nickels
+        case (cents >= 05):
+            var nickels = Math.floor(cents / 05);
+            coins[2] = nickels;
+            cents -= (nickels * 05);
+        //pennies
+        case (cents >= 01):
+            var pennies = Math.floor(cents / 01);
+            coins[3] = pennies;
+            cents = 0;
+            // cents -= (pennies * 01);
+            break;
     }
-    while (cents > 0);
 
-
-
-console.log(coins);
     return coins;
 
 };
+
+$(document).ready(function() {
+
+    $('form#change-input').submit(function(event) {
+
+        var change = $('input#change').val();
+        var result = coinCombo(change);
+
+        if (result[0] > 0) {
+            $('#p-quarters').show();
+            $('#quarters').show();
+        }
+
+        if (result[1] > 0) {
+            $('#p-dimes').show();
+            $('#dimes').show();
+        }
+
+        if (result[2] > 0) {
+            $('#p-nickels').show();
+            $('#nickels').show();
+        }
+
+        if (result[3] > 0) {
+            $('#p-pennies').show();
+            $('#pennies').show();
+        }
+
+        $("#quarters").text(result[0]);
+        $("#dimes").text(result[1]);
+        $("#nickels").text(result[2]);
+        $("#pennies").text(result[3]);
+
+        $('#result').show();
+
+        event.preventDefault();
+
+
+    })
+
+
+});
